51907ef1997ec3366865749b478d70f854390d2d
Ricardo Koiti Matsushita <127707509+matsushitakoiti@users.noreply.github.com>
2025-07-23 21:54:03 -0300

    Add files via upload
    Inclusão das depedências de Auth e JWT e criação de services de usuario

--- reservei/src/main/java/com/senai/reservei/model/Usuario.java
+++ reservei/src/main/java/com/senai/reservei/model/Usuario.java
@@ -1,16 +1,58 @@
 package com.senai.reservei.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String usuario;
-    private String password;
+    private String senha;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("USUARIO COMUM"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.usuario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
---
+++ reservei/src/main/java/com/senai/reservei/service/TokenService.java
@@ -0,0 +1,49 @@
+package com.senai.reservei.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.senai.reservei.model.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String chaveSecreta;
+
+    public String generatedToken(Usuario usuario) {
+        try{
+            Instant tempoExpiracao = LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03.00"));
+            return JWT.create()
+                    .withIssuer("reservei")
+                    .withSubject(usuario.getId().toString())
+                    .withExpiresAt(tempoExpiracao)
+                    .sign(Algorithm.HMAC256(chaveSecreta));
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Não foi possível gerar o token JWT", e);
+        }
+    }
+
+    public String validateToken(String token){
+        try{
+            return JWT.require(Algorithm.HMAC256(chaveSecreta))
+                    .withIssuer("reservei")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        } catch (JWTVerificationException e) {
+            throw new RuntimeException("Token invalido", e);
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
